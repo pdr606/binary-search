b312683207cf2ea475d09d3c9ed03ea312edce15
Pedro Guarnieri <pedroguarnieric@gmail.com>
2023-10-21 00:55:47 -0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,33 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args){
+
+        int[] ints = {1, 2, 4, 5, 7, 9, 11};
+
+        System.out.println(binarySearch(ints, 9));
+
+
+    }
+
+    private static int binarySearch(int[] numbers, int numberToFind){
+        int low = 0;
+        int high = numbers.length - 1;
+
+        while(low <= high){
+            int middlePosition = (low + high) / 2;
+            int middleNumber = numbers[middlePosition];
+
+            if(numberToFind == middleNumber){
+                return middlePosition;
+            }
+            if(numberToFind < middleNumber){
+                high = middlePosition - 1;
+            }
+            else {
+                low = middlePosition + 1;
+            }
+        }
+        return -1;
+    }
+}
